fb25c9f0a36578c3c44100d375ddf8f3960e1ace
Rakibul Islam <rakibulislam4001@gmail.com>
2021-08-06 23:25:25 +0600

    hi

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var num = [5, 6, 8, 9, 3, 7, 2];
+// var num = [5, 6, 8, 9, 3, 7, 2];
 // console.log(num.length);
 // 1
 // num.shift(1);
@@ -34,15 +34,15 @@ var num = [5, 6, 8, 9, 3, 7, 2];
 // var reg = "reg";
 // createObject[reg] = "258963";
 // console.log(createObject);
-
-// function celsiusToFahrenheit(cel) {
-//     var cel = (cel - 32) * 9;
-//     return cel;
-// }
-// // var cel = parseInt(Math.random() * 50);
-// var cel = 20;
-// var fahrenheit = celsiusToFahrenheit(cel);
-// console.log(fahrenheit);
+// (0°C × 9/5) + 32 = 32°F formula
+/* function celsiusToFahrenheit(cel) {
+    var cel = ((cel * 9)/5) + 32;
+    return cel;
+}
+// var cel = parseInt(Math.random() * 50);
+var cel = parseInt(Math.random() * 50);
+var fahrenheit = celsiusToFahrenheit(cel);
+console.log(`${cel}°C = ${fahrenheit}°F`); */
 
 // multi line comment shift+alt+A
 /* var randomNumber = parseInt(Math.random() * 9);
@@ -52,3 +52,13 @@ if (randomNumber == 7 || randomNumber == 5) {
     console.log("Opps!!! Wrong number");
 }
 console.log(`correct number is = ${randomNumber}`); */
+let num = 56;
+let str = 'Hello';
+let bool = true;
+/* if (num == 56) {
+    console.log("this is number");
+} */
+
+if(num != "Hello"){
+    console.log("this is not correct");
+}
