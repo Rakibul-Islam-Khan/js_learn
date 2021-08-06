e1c092f4ca210b073ae0e7b1ffa9525c4f094bd6
Rakibul Islam <rakibulislam4001@gmail.com>
2021-08-06 20:32:04 +0600

    new

---
+++ test.js
@@ -0,0 +1,54 @@
+var num = [5, 6, 8, 9, 3, 7, 2];
+// console.log(num.length);
+// 1
+// num.shift(1);
+// console.log(num);
+// // 2
+// // num.push(1);
+// // console.log(num);
+// // 3
+// num.unshift(8);
+// console.log(num);
+// 4
+// num.pop(3);
+// console.log(num);
+// // 5
+// num[3] = "new";
+// console.log(num);
+// // 6
+// var str = true;
+// var num = false;
+// console.log(typeof str,typeof num);
+// 7
+// var createObject = {
+//     student_id:20568,
+//     roll:895,
+//     reg:025868,
+//     name:"monir",
+//     technology:"computer",
+//     gender:"male",
+//     age:25
+// }
+// createObject.name = "mike";
+// createObject["age"] = 30;
+// var reg = "reg";
+// createObject[reg] = "258963";
+// console.log(createObject);
+
+// function celsiusToFahrenheit(cel) {
+//     var cel = (cel - 32) * 9;
+//     return cel;
+// }
+// // var cel = parseInt(Math.random() * 50);
+// var cel = 20;
+// var fahrenheit = celsiusToFahrenheit(cel);
+// console.log(fahrenheit);
+
+// multi line comment shift+alt+A
+/* var randomNumber = parseInt(Math.random() * 9);
+if (randomNumber == 7 || randomNumber == 5) {
+    console.log("your guess is correct");
+} else {
+    console.log("Opps!!! Wrong number");
+}
+console.log(`correct number is = ${randomNumber}`); */
